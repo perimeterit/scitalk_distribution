d18256b83cddcad873892d76c27f8a340e04b612
Luis Cabo <lrivas@perimeterinstitute.ca>
2020-08-26 13:56:23 -0400

    added patch to suppert computed fields in layout manager, updated computed field to check if the fields are visible before computing them

--- profiles/scitalk/modules/custom/scitalk_base/src/Plugin/Field/SciTalkMostRecentTalkList.php
+++ profiles/scitalk/modules/custom/scitalk_base/src/Plugin/Field/SciTalkMostRecentTalkList.php
@@ -6,6 +6,7 @@ use Drupal\Core\Field\FieldItemList;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\TypedData\ComputedItemListTrait;
 
+
 class SciTalkMostRecentTalkList extends FieldItemList implements FieldItemListInterface {
   
   use ComputedItemListTrait;
@@ -17,7 +18,15 @@ class SciTalkMostRecentTalkList extends FieldItemList implements FieldItemListIn
   protected function computeValue() {
     $entity = $this->getEntity();
     $bundle = $entity->bundle();
- 
-    $this->list[0] = $this->createItem(0, $entity->id());
+
+    $entity_view_id = "taxonomy_term.{$bundle}.default";
+    $view_mode = \Drupal::entityManager()->getStorage('entity_view_display')->load($entity_view_id);
+    //$visible_fields = $view_mode ? $view_mode->get('content') : [];
+    $hidden_fields = $view_mode ? $view_mode->get('hidden') : [];
+    
+    //only compute if this field is enabled/visible in the default entity view display for series or collections:
+    if (!in_array('scitalk_most_recent_talk', $hidden_fields)) {
+      $this->list[0] = $this->createItem(0, $entity->id());
+    }
   }
 }
