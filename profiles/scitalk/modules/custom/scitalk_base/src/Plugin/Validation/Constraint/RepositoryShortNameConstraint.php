238a284c8996c01d67f711e230833b85c90d8359
Luis Cabo <lrivas@perimeterinstitute.ca>
2020-11-27 17:20:43 -0500

    added constraint for repository short name max length

---
+++ profiles/scitalk/modules/custom/scitalk_base/src/Plugin/Validation/Constraint/RepositoryShortNameConstraint.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\scitalk_base\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * Checks that the submitted value is a unique ID.
+ *
+ * @Constraint(
+ *   id = "RepositoryShortNameMaxLength",
+ *   label = @Translation("Repository Short Name length contraint", context = "Validation"),
+ *   type = "entity"
+ * )
+ */
+class RepositoryShortNameConstraint extends Constraint {
+
+  // The message that will be shown if the value is not unique.
+  public $tooLong = 'Repository Short Name must be 20 characters long or less';
+
+}
---
+++ profiles/scitalk/modules/custom/scitalk_base/src/Plugin/Validation/Constraint/RepositoryShortNameConstraintValidator.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\scitalk_base\Plugin\Validation\Constraint;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+/**
+ * Validates the Repository Short Name constraint.
+ */
+class RepositoryShortNameConstraintValidator extends ConstraintValidator {
+  const MAX_LEN = 20;
+  private $entity_type = 'source_repository';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validate($item, Constraint $constraint) {
+    if (!isset($item)) {
+      return;
+    }
+    $entity = $item->getEntity();
+    if ($entity->bundle() == $this->entity_type) {
+      
+      $short_repo_name_len = strlen($item->value) ?? 0;
+      if ($short_repo_name_len > self::MAX_LEN) {
+        $this->context->addViolation( $constraint->tooLong, ['%value' => $item->value] );
+      }
+    }
+  }
+
+}
