a0307eca6962c67caed4d427c1c48cb085520218
Megan McDermott <megan@giantgoat.com>
2021-03-10 10:40:41 -0500

    Added theming for new advanced search (view and form).

--- profiles/scitalk/themes/scitalk_theme/js/global.js
+++ profiles/scitalk/themes/scitalk_theme/js/global.js
@@ -1,19 +1,47 @@
 
+(function ($) {
 /**
  * Responsive tables for Latest Talks table display
 */
-(function ($) {
   Drupal.behaviors.responsiveTable = {
     attach: function (context, settings) {
       $.responsiveTables('800px');
     }
   };
-})(jQuery);
 
-/**
- * Related talks
-*/
-(function (Drupal) {
+  Drupal.behaviors.toggleAdvanced = {
+    attach: function (context, settings) {
+      $adv_btn = '<a class="toggle-advanced toggle-expand" href="#" title="Toggle Advanced Search">Show advanced</a>';
+      $adv_search_el = $('.block-views-exposed-filter-block--scitalk-advanced-search-advanced-search');
+      $basic_search_el = $('#views-exposed-form-scitalk-advanced-search-basic-search');
+
+      $adv_search_el.hide();
+      $('#views-exposed-form-scitalk-advanced-search-basic-search .form-actions', context).append($adv_btn);
+      $('.search-wrap', context).prepend('<span class="hide-advanced toggle-expand toggle-contract">Hide advanced</span>');
+
+      $('.toggle-advanced', context).click( function(e){
+        e.preventDefault();
+
+        $adv_search_el.slideDown();
+        $basic_search_el.slideUp();
+
+        // Prepopulate search & subjects fields with values from simple search bar
+        $form_search_val = $('.form-item-search .form-text', $basic_search_el).val();
+        $form_search_subject = $('.form-item-talk-subject .form-select', $basic_search_el).val();
+        $('.form-item-search .form-text', $adv_search_el).val($form_search_val);
+        $('.form-item-talk-subject .form-select', $adv_search_el).val($form_search_subject);
+      });
+
+      $('.hide-advanced', context).click(function(){
+        $adv_search_el.slideUp();
+        $basic_search_el.slideDown();
+      });
+
+    }
+  };
+  /**
+   * Related talks
+  */
   Drupal.behaviors.toggleRelatedPosts = {
     attach: function (context, settings) {
 
@@ -28,5 +56,4 @@
       }
     }
   };
-})(Drupal);
-
+}(jQuery));
