890570f586bfab845c370ed92f3854b7e3857c4a
Randy Kolenko <randy@kolenko.org>
2021-04-07 17:02:41 -0400

    Fix for Collection unique constraint to use label vs. title in entity.

--- profiles/scitalk/modules/custom/scitalk_base/src/Plugin/Validation/Constraint/UniqueCollectionNumberConstraintValidator.php
+++ profiles/scitalk/modules/custom/scitalk_base/src/Plugin/Validation/Constraint/UniqueCollectionNumberConstraintValidator.php
@@ -8,8 +8,7 @@ use Symfony\Component\Validator\ConstraintValidator;
 /**
  * Validates the UniqueCollectionNumber constraint.
  */
-class UniqueCollectionNumberConstraintValidator extends ConstraintValidator { 
-  private $vocabulary_name = 'collection';
+class UniqueCollectionNumberConstraintValidator extends ConstraintValidator {
 
   /**
    * {@inheritdoc}
@@ -17,32 +16,32 @@ class UniqueCollectionNumberConstraintValidator extends ConstraintValidator {
   public function validate($entity, Constraint $constraint) {
     //Determine what entity type this is.
     $thisEntity = $entity->getEntity();
-    
+
     if (!isset($thisEntity)) {
       return;
     }
 
     $entityType = $thisEntity->getEntityTypeId();
-    
+
     if($entityType == 'node') {
       //for the node collection type, load the source and talk
       $source_target_id = $thisEntity->get('field_collection_source_repo')->target_id ?? 0;
       $source_field = \Drupal::entityTypeManager()->getStorage('node')->load($source_target_id);
       $source_name = $source_field->title->value ?? '';
-      
+
       //Make sure that (in case they are changing the talk number) that no other entity has the same colletion number
       if ($thisEntity->id()) {
-        $isUnique = $this->isUnique( $thisEntity->field_collection_number->value, $source_name, $thisEntity->id() );
+        $isUnique = $this->isUnique( $thisEntity->field_collection_number->value, $source_name, $thisEntity->id());
       }
       else {
-        $isUnique = $this->isUnique( $thisEntity->field_collection_number->value, $source_name, '', $entityType);
+        $isUnique = $this->isUnique( $thisEntity->field_collection_number->value, $source_name, '');
       }
-      
+
       if (!$isUnique) {
         $this->context->addViolation( t($constraint->notUnique, ['%value' => $thisEntity->field_collection_number->value]) );
       }
     }
-   
+
   }
 
   /**
@@ -50,17 +49,17 @@ class UniqueCollectionNumberConstraintValidator extends ConstraintValidator {
    *
    * @param string $value
    */
-  private function isUnique($value, $source_name, $id = '', $entityType) {
+  private function isUnique($value, $source_name, $id = '') {
     $query_count = \Drupal::entityQuery('node')
       ->condition('type', 'collection')
       //->condition('status', 1)  //omitting as there could be a valid collection that is not published
       ->condition('field_collection_number', $value, '=');
-      
-    
+
+
     if (!empty($source_name)) {
-      $query_count->condition('field_collection_source.entity.title', $source_name);
+      $query_count->condition('field_collection_source_repo.entity.label', $source_name);
     }
-    
+
     if (!empty($id)) {
       $query_count->condition('nid', $id, '<>');
     }
