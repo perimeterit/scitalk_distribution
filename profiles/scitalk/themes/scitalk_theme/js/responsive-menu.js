24a0d30fcb4b08b5dc2147a1f8e3b52101cb0dfb
Megan McDermott <megan@giantgoat.com>
2022-02-24 17:30:30 -0500

    Fixed the way the responsive menu works to address issues with search form on Android.

--- profiles/scitalk/themes/scitalk_theme/js/responsive-menu.js
+++ profiles/scitalk/themes/scitalk_theme/js/responsive-menu.js
@@ -3,47 +3,17 @@
   Drupal.behaviors.scitalk_mobile_menus = {
    attach: function (context, settings) {
 
-     // 'push_left' also works here
-     menu_setting = 'push_right';
-     menu_breakpoint = 959.999;
-
-     if (context == document) {
-       // Get all the elements we need to put into the mobil emenu
-       nav_element = $('.left-nav-wrapper');
-     }
+     // Take out the DOM rearranging. We don't need it now. Instead just use CSS
+     // And JS is only needed to toggle it on and off.
      // Build the menu if the browser with is smaller than the breakpoint
-     if (($(window).width() <= menu_breakpoint) && ($('.region-branding .menu-toggle').length == 0)) {
-       buildMenu();
-     }
+      $('.left-nav-wrapper', context).prepend('<button class="menu--close menu--icon" aria-expanded="true"><span class="visually-hidden">' + $('.menu-toggle span').html() +  '</span></button>');
 
-     // Make the mobile menu
-     function buildMenu() {
-       // Add classes to the body tag
-       $('body').addClass('responsive-menu-active menu--' + menu_setting);
-       // Create an empty div for the menu
-       if ($('#menu-container').length == 0) {
-         $('body').prepend('<div id="menu-container" class="responsive-menu menu--' + menu_setting + ' " tabindex="-1"></div>');
-       }
-       // create the close toggle element
-       if ($('#menu-container .menu--close').length == 0) {
-         $('#menu-container', context).prepend('<button class="menu--close menu--icon" aria-expanded="true"><span class="visually-hidden">' + $('.menu-toggle span').html() +  '</span></button>');
-       }
-
-       // Move the blocks
-       nav_element.detach().appendTo($('#menu-container'));
-     }
-
-     // Put the menu back to the standard (desktop) layout
-     function destroyMenu() {
-       nav_element.detach();
-       nav_element.prependTo($('.page-wrapper'));
-       $('body').removeClass('responsive-menu-active');
-     }
-
-     // Toggle the menu open/closed, from the hamburger icon or the close <
+     // Toggle the menu open/closed, from the hamburger icon or the close
      $menu_toggle_buttons = $('.menu-toggle, .menu--close', context);
      $($menu_toggle_buttons).on('click', function() {
+       console.log('toggle menu');
        if ($(this).attr('aria-expanded') == 'false') {
+         // Open the menu
          $menu_toggle_buttons.attr('aria-expanded', 'true');
          // Also moves the focus to the first link item (for screen readers)
          $('#menu-container .menu--main').attr('tabindex','-1').focus();
@@ -56,26 +26,9 @@
          $('.menu-toggle').focus();
          $menu_toggle_buttons.children('span').html($toggle_text);
        }
-       $('body, #menu-container', context).toggleClass('menu--open');
+       $('body', context).toggleClass('menu--open');
      });
 
-     // Fix layout when browser window is resized
-     var resizeId;
-     $(window).on('resize', function() {
-       clearTimeout(resizeId);
-       resizeId = setTimeout(doneResizing, 700);
-     });
-
-     function doneResizing() {
-       // If it's smaller than the mobile menu breakpoint and the menu toggle doesn't exist, make it
-       if (($(window).width() <= menu_breakpoint) && ($('.region-branding .menu-toggle').length == 0)) {
-         buildMenu();
-       }
-       // Otherwise, if it's greater than the menu breakpoint and ???
-       else if ($(window).width() > menu_breakpoint) {
-         destroyMenu($('.menu-region'));
-       }
-     }
    }
   };
 
