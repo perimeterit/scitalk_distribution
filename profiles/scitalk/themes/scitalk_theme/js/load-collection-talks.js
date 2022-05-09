0d2ece4ecf83d0622e481766e874564886ac1d5b
Megan McDermott <megan@woolwichweb.works>
2022-05-09 10:50:28 -0400

    Additional work on Collections UX (theming).

--- profiles/scitalk/themes/scitalk_theme/js/load-collection-talks.js
+++ profiles/scitalk/themes/scitalk_theme/js/load-collection-talks.js
@@ -1,34 +1,56 @@
 (function ($) {
-  Drupal.behaviors.scitalkAjaxLoad = {
+  Drupal.behaviors.scitalkLoadChildren = {
     attach: function (context, settings) {
-      // Attach ajax action click event of each view column.
-      $('.node.collection .show-children', context).each(this.attachLink);
-      $('.collection-wrapper + .hide-children').click(function() {
-        $(this).siblings('.advanced-view-display').slideUp()
-        ;
-      })
+
+
+    $('.collection .toggle-children', context).click(function() {
+      var collection_id = $(this).data('collection-id');
+      // If it's currently closed
+      if ($(this).attr('aria-expanded') == 'false') {
+        // Close the other ones
+        $(".collection-children").attr('aria-hidden', 'true');
+        // Expand this one
+        $(this).attr('aria-expanded', 'true');
+        $(this).children('span').html('Hide');
+        $('#collection-children--' + collection_id).attr('aria-hidden','false');
+      }
+      // If it's currently open
+      else {
+        $(this).attr('aria-expanded', 'false');
+        $(this).children('span').html('Show');
+        $('#collection-children--' + collection_id).attr('aria-hidden','true');
+      }
+    })
+
+      // Attach ajax action click event of each show more button
+      // $('.node.collection .show-children', context).each(this.loadMore);
+
+
     },
 
-    attachLink: function (idx, button) {
-      // Get the Node id
-      var nid = $(this).data('node-id');
-      // Everything we need to specify about the view.
-      var view_info = {
-        view_name: 'scitalk_collection_children',
-        view_display_id: 'card_view',
-        view_args: nid + '/' + nid,
-        view_dom_id: 'collection-children-' + nid
-      };
+    // Load by ajax - keep this for awhile in case we want to try this again
+    // loadMore: function (idx, button, context) {
+    //   console.log('load more');
+    //   // Get the Node id
+    //   var nid = $(this).data('node-id');
+    //   // Everything we need to specify about the view.
+    //   var view_info = {
+    //     view_name: 'scitalk_collection_children',
+    //     view_display_id: 'card_view',
+    //     view_args: nid + '/' + nid,
+    //     view_dom_id: 'collection-children-' + nid
+    //   };
+    //
+    //   // Details of the ajax action.
+    //   var ajax_settings = {
+    //     submit: view_info,
+    //     url: '/views/ajax',
+    //     element: button,
+    //     event: 'click'
+    //   };
+    //
+    //   Drupal.ajax(ajax_settings);
+    // }
 
-      // Details of the ajax action.
-      var ajax_settings = {
-        submit: view_info,
-        url: '/views/ajax',
-        element: button,
-        event: 'click'
-      };
-
-      Drupal.ajax(ajax_settings);
-    }
   };
 })(jQuery);
