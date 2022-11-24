4e883b422c178cdbd3c5daa4884f82e30fb726e1
Luis Cabo <lrivas@perimeterinstitute.ca>
2022-11-24 12:55:43 -0500

    replace Collections Most recent Talk and Number of Talks computed fields with regular fields

---
+++ profiles/scitalk/modules/custom/scitalk_base/src/ScitalkServices/CollectionTalksStats.php
@@ -0,0 +1,81 @@
+<?php
+namespace Drupal\scitalk_base\ScitalkServices;
+
+use Drupal\Core\Entity\EntityInterface;
+
+class CollectionTalksStats {
+   
+    public function update(EntityInterface $entity) {
+        $collection_nid = $entity->get('field_talk_collection')->target_id ?? '';
+        \Drupal::logger('scitalk_base')->notice('<pre><code>Up here '. print_r($entity->toArray(), TRUE) . '</code></pre>');
+
+        if (empty($collection_nid)) {
+            return;
+        }
+
+        $number_of_talks = $this->fetchNumberOfTalks($collection_nid);
+        $most_recent_talk = $this->fetchMostRecentTalkDate($collection_nid);
+
+        $collection = \Drupal::entityTypeManager()->getStorage('node')->load($collection_nid);
+        $collection->set('field_collection_number_of_talks', $number_of_talks);
+        $collection->set('field_collection_last_talk_date', $most_recent_talk);
+
+        $collection->save();
+
+        \Drupal::logger('scitalk_base')->notice(
+            '<pre><code>Update Number of talks: '.$number_of_talks . ', most recent: '. $most_recent_talk .  print_r($entity->toArray(), TRUE) . 
+            '</code></pre>'
+        );
+    }
+
+    /**
+     * return the number of talks under a Collection or Series
+     */
+    private function fetchNumberOfTalks($nid) {
+        $entity = \Drupal::entityTypeManager()->getStorage('node')->load($nid);
+        $bundle = $entity->bundle();
+        
+        //query number of talks for a collection
+        $query_count = \Drupal::entityQuery('node')
+            ->condition('type', 'talk')
+            ->condition('status', 1)
+            ->condition('field_talk_collection.target_id', $nid);
+
+        return  $query_count->count()->execute() ?? 0;
+    }
+
+     /**
+    * return the date for the most recent talk under a Collection
+    */
+   private static function fetchMostRecentTalkDate($nid) {
+        $entity = \Drupal::entityTypeManager()->getStorage('node')->load($nid);
+        $bundle = $entity->bundle();
+
+        //query most recent talk for a collection or series 
+        $query = \Drupal::entityQuery('node')
+            ->condition('type', 'talk')
+            ->condition('status', 1)
+            ->condition('field_talk_collection.target_id', $nid);
+
+        $talk = $query->sort('field_talk_date','DESC')
+            ->range(0,1)
+            ->execute();
+
+        $most_recent_date = '';
+        $date_format = 'Y-m-d\TH:i:s';
+        if ($talk) {
+            $talk_nid = current($talk);
+            $talk_entity = \Drupal::entityTypeManager()->getStorage('node')->load($talk_nid);
+            
+            $most_recent_date = $talk_entity->field_talk_date->value ?? '';
+
+            // if (!empty($most_recent_date)) {
+            //     $tz = new \DateTimeZone( \Drupal::currentUser()->getTimezone());
+            //     $utc = new \DateTimeZone("UTC");
+            //     $most_recent_date =  (new \Drupal\Core\Datetime\DrupalDateTime($most_recent_date, $utc))->setTimezone($tz)->format($date_format);
+            // }
+        }
+
+        return $most_recent_date;
+    }
+}
